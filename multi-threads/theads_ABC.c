aeb6913fd92c79ac5861cfa10f1a7f5601845242
Lucas Diego Reboucas Rocha <lucas.diegorr@gmail.com>
2015-07-02 23:35:54 -0300

    Melhoria das funções de inserção de caracteres;

--- multi-threads/theads_ABC.c
+++ multi-threads/theads_ABC.c
@@ -10,49 +10,55 @@ bool lockB;
 bool lockC;
 bool alive;
 
-void *functionA(void *args) {
-  while(alive) {
-    
-    while(lockA);
-    array_char[index++] = 'A';
-    lockA = true;
-    lockB = false;
+void print_array() {
+  int index_aux;
+  for (index_aux = 0; index_aux < 30; index_aux++){
+    printf("%d: %c\n", index_aux, array_char[index_aux]);
   }
-  pthread_exit(0);
 }
 
-void *functionB(void *args) {
-  while(alive) {
-    
-    while(lockB);
-    array_char[index++] = 'B';
-    lockB = true;
-    lockC = false;
-  }
-  pthread_exit(0);
-}
+void *insert_character_function( void *element ){
+     char *character;
+     character = (char *) element;
+     
+     while(alive) {
+         
+         if (*character == 'A'){
 
-void *functionC(void *args) {
-  
-  while(alive) {
-    
-    while(lockC);
-    array_char[index++] = 'C';
-    lockC = true;
-    lockA = false;
+          while(lockA);
+          array_char[index++] = 'A';
+          lockA = true;
+          lockB = false;
 
-    if (index > 30){
-      int index_aux;
-      for (index_aux = 0; index_aux < 30; index_aux++){
-        printf("%d: %c\n", index_aux, array_char[index_aux]);
-      }
-      alive = false;
-      lockA = false;
-      lockB = false;
-      pthread_exit(0);
-    }
+         } else if(*character == 'B') {
 
-  }
+          while(lockB);
+          array_char[index++] = 'B';
+          lockB = true;
+          lockC = false;
+
+         }else {
+
+          while(lockC);
+          array_char[index++] = 'C';
+          lockC = true;
+          lockA = false;
+         
+          if (index > 30){
+            
+            print_array();
+
+            alive = false;
+            lockA = false;
+            lockB = false;
+          }
+
+         }
+
+     }
+
+     pthread_exit(0);
+
 }
 
 int main(int argc, char *argv[]){
@@ -68,16 +74,13 @@ int main(int argc, char *argv[]){
    lockC = true;
    index = 0;
 
-   pthread_create( &threadA, NULL, functionA, (void*) element_A );
-   pthread_create( &threadB, NULL, functionB, (void*) element_B );
-   pthread_create( &threadC, NULL, functionC, (void*) element_C );
+   pthread_create( &threadA, NULL, insert_character_function, (void*) element_A );
+   pthread_create( &threadB, NULL, insert_character_function, (void*) element_B );
+   pthread_create( &threadC, NULL, insert_character_function, (void*) element_C );
 
    pthread_join( threadA, NULL);
    pthread_join( threadB, NULL);
    pthread_join( threadC, NULL);
    
-   printf("Fim do Exercício\n");
-
    return 0;
 }
- 
