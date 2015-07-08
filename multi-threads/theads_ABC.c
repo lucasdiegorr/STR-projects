766142fa15643154f052aeb25c226ede409de2da
Lucas Diego <lucas.diegorr@gmail.com>
2015-07-08 00:19:29 -0300

    AdiÃ§Ã£o de port para Windows

--- multi-threads/theads_ABC.c
+++ multi-threads/theads_ABC.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
+
+#if defined(__linux__)
+  #include <pthread.h>
+#else
+  #include <process.h>
+#endif
 
 char array_char[30];
-int index;
+int index_array;
 bool lockA;
 bool lockB;
 bool lockC;
@@ -17,7 +22,7 @@ void print_array() {
   }
 }
 
-void *insert_character_function( void *element ){
+void insert_character_function( void *element ){
      char *character;
      character = (char *) element;
      
@@ -26,25 +31,25 @@ void *insert_character_function( void *element ){
          if (*character == 'A'){
 
           while(lockA);
-          array_char[index++] = 'A';
+          array_char[index_array++] = 'A';
           lockA = true;
           lockB = false;
 
          } else if(*character == 'B') {
 
           while(lockB);
-          array_char[index++] = 'B';
+          array_char[index_array++] = 'B';
           lockB = true;
           lockC = false;
 
          }else {
 
           while(lockC);
-          array_char[index++] = 'C';
+          array_char[index_array++] = 'C';
           lockC = true;
           lockA = false;
          
-          if (index > 30){
+          if (index_array > 30){
             
             print_array();
 
@@ -56,31 +61,47 @@ void *insert_character_function( void *element ){
          }
 
      }
-
-     pthread_exit(0);
-
 }
 
 int main(int argc, char *argv[]){
 
-   pthread_t threadA, threadB, threadC;
-   const char *element_A = "A";
-   const char *element_B = "B";
-   const char *element_C = "C";
+  #if defined(__linux__)
+    pthread_t threadA, threadB, threadC;
+  #else
+    int threadA, threadB, threadC;
+  #endif
 
-   alive = true;
-   lockA = false;
-   lockB = true;
-   lockC = true;
-   index = 0;
+  const char *element_A = "A";
+  const char *element_B = "B";
+  const char *element_C = "C";
 
-   pthread_create( &threadA, NULL, insert_character_function, (void*) element_A );
-   pthread_create( &threadB, NULL, insert_character_function, (void*) element_B );
-   pthread_create( &threadC, NULL, insert_character_function, (void*) element_C );
+  alive = true;
+  lockA = false;
+  lockB = true;
+  lockC = true;
+  index_array = 0;
 
-   pthread_join( threadA, NULL);
-   pthread_join( threadB, NULL);
-   pthread_join( threadC, NULL);
-   
-   return 0;
+  #if defined(__linux__)
+    pthread_create( &threadA, NULL, insert_character_function, (void*) element_A );
+    pthread_create( &threadB, NULL, insert_character_function, (void*) element_B );
+    pthread_create( &threadC, NULL, insert_character_function, (void*) element_C );
+  #else
+    if ((threadA = _beginthread(insert_character_function, 4096, (void*) element_A)) == -1){
+      printf("Erro na threadA.");
+    }
+    if ((threadB = _beginthread(insert_character_function, 4096, (void*) element_B)) == -1){
+      printf("Erro na threadB.");
+    }
+    if ((threadC = _beginthread(insert_character_function, 4096, (void*) element_C)) == -1){
+      printf("Erro na threadC."); 
+    }
+  #endif
+
+  #if defined(__linux__)
+    pthread_join( threadA, NULL);
+    pthread_join( threadB, NULL);
+    pthread_join( threadC, NULL);
+  #endif
+
+  return 0;
 }
