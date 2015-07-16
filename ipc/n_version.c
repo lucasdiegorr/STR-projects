ec55a91ae1d5ef011f21f2fa30c8dd18231f8cb3
Lucas Diego <lucas.diegorr@gmail.com>
2015-07-16 19:31:23 -0300

    Finalizado 4 exercicio;

--- ipc/n_version.c
+++ ipc/n_version.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 int votes[3];
-int array_channels[6] = {-1};
+int array_channels[6] = {-1, -1, -1, -1, -1, -1};
 bool alive;
 
 void send_message_asynchronous(int *buffered_value, int channel){
@@ -21,33 +21,68 @@ void receive_message_synchronous(int *buffered_value, int channel){
 }
 
 void function_version_0(void *params) {
+  bool thread_alive = true;
   int vote = 5 + 5;
   send_message_asynchronous(&vote, 0);
 
-  int singnal;
-  receive_message_synchronous(&singnal, 3);
+  int signal;
+  receive_message_synchronous(&signal, 3);
 
-  while(alive);
+  printf("A thread 0 recebeu o sinal %d\n", signal);
+
+  if (signal == 1){
+    printf("A thread 0 recebeu ordem para continuar mudando o voto que sera %d.\n", signal);
+
+  }else if(signal == 2){
+    printf("A thread 0 recebeu ordem de parar.\n");
+  }else {
+    printf("A thread 0 recebeu ordem de continuar a execucao.\n");
+  }
+
+  while(alive && thread_alive);
 }
 
 void function_version_1(void *params) {
+  bool thread_alive = true;
   int vote = 2 * 5;
   send_message_asynchronous(&vote, 1);
 
-  int singnal;
-  receive_message_synchronous(&singnal, 4);
+  int signal;
+  receive_message_synchronous(&signal, 4);
 
-  while(alive);
+  printf("A thread 1 recebeu o sinal %d\n", signal);
+
+  if (signal == 1){
+    printf("A thread 1 recebeu ordem para continuar mudando o voto que sera %d.\n", signal);
+    vote = signal;
+  }else if(signal == 2){
+    printf("A thread 1 recebeu ordem de parar.\n");
+  }else {
+    printf("A thread 1 recebeu ordem de continuar a execucao.\n");
+  }
+
+  while(alive && thread_alive);
 }
 
 void function_version_2(void *params) {
-  int vote = 3 + 7;
+  bool thread_alive = true;
+  int vote = 3 + 6;
   send_message_asynchronous(&vote, 2);
 
-  int singnal;
-  receive_message_synchronous(&singnal, 5);
+  int signal;
+  receive_message_synchronous(&signal, 5);
 
-  while(alive);
+  printf("A thread 2 recebeu o sinal %d\n", signal);
+
+  if (signal == 1){
+    printf("A thread 2 recebeu ordem para continuar mudando o voto que sera %d.\n", signal);
+  }else if(signal == 2){
+    printf("A thread 2 recebeu ordem de parar.\n");
+  }else {
+    printf("A thread 2 recebeu ordem de continuar a execucao.\n");
+  }
+
+  while(alive && thread_alive);
 }
 
 int create_thread(void *function) {
@@ -96,8 +131,8 @@ int mode(int array[], int length) {
 
 void driver(void *params) {
 
-  int array_comparison[3] = {0};
-  int majority_vote;
+  int array_comparison[3] = {0,0,0};
+  int majority_vote, index, signal_return;
 
   create_thread(function_version_0);
   create_thread(function_version_1);
@@ -109,10 +144,26 @@ void driver(void *params) {
 
   majority_vote = mode(array_comparison, 3);
 
+  printf("O voto majoritario foi %d\n", majority_vote);
+
+  for(index = 0; index < 3; index++){
+    if(array_comparison[index] == majority_vote){
+      signal_return = 0;
+    }else if((array_comparison[index] >= (majority_vote - 1)) && (array_comparison[index] <= (majority_vote + 1))){
+      signal_return = 1;
+    }else{
+      signal_return = 2;
+    }
+    printf("Sinal para a thread %d foi %d.\n", index, signal_return);
+    send_message_asynchronous(&signal_return, index+3);
+  }
+  alive = false;
 }
 
 int main(int argc, char const *argv[]){
 
+  alive = true;
+  create_thread(driver);
   system("PAUSE");
   return 0;
 }
